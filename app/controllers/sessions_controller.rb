a0e984addceb7fec71537ea4907603c600c3edc7
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2020-03-15 15:56:03 -0600

    login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+
+  def new; end
+
+  def create
+    @user = User.find_by(email_address: params[:email_address])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to '/welcome'
+    else
+       redirect_to '/login'
+    end
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to '/welcome'
+  end
+
+  def login; end
+
+  def welcome; end
+
+  def page_requires_login; end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :recommended, class_name: "Recommendation"
   has_many :users_recommendations
   has_many :recommendations, through: :users_recommendations
+
+  def username
+    email_address.split(/@/).first
+  end
 end
+
